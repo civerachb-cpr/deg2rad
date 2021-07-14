a50ed2ea0e8000e106667aa1241d6ed7c0c4c135
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2021-07-14 15:23:30 -0400

    Add a script to make a .deb package

---
+++ mkpkg.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+checkinstall -D --pkgname deg2rad --pkgversion 0.1 --pkgrelease 1 --pkglicense bsd --arch all --pkggroup devel --maintainer civerachb@clearpathrobotics.com --pkgsource https://github.com/civerachb-cpr/deg2rad --provides deg2rad,rad2deg --install ./install.sh
+
