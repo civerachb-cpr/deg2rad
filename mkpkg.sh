d37c8c3c4b05fc1bff878e43bc716878f6e4f2ed
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2022-04-14 13:50:54 -0400

    Use Python3

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-checkinstall -D --pkgname deg2rad --pkgversion 0.1 --pkgrelease 1 --pkglicense bsd --arch all --pkggroup devel --maintainer civerachb@clearpathrobotics.com --pkgsource https://github.com/civerachb-cpr/deg2rad --provides deg2rad,rad2deg --install ./install.sh
+checkinstall -D --pkgname deg2rad --pkgversion 0.2 --pkgrelease 1 --pkglicense bsd --arch all --pkggroup devel --maintainer civerachb@clearpathrobotics.com --pkgsource https://github.com/civerachb-cpr/deg2rad --provides deg2rad,rad2deg --install ./install.sh
 
