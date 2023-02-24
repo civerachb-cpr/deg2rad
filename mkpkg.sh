b756d6fd76de9108589900dd9f471a5d2bd66775
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2023-02-24 14:07:43 -0500

    Use math.degrees or math.radians instead of multiplying/dividing by pi. Same thing under the hood, but this is tidier.

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-checkinstall -D --pkgname deg2rad --pkgversion 0.2 --pkgrelease 1 --pkglicense bsd --arch all --pkggroup devel --maintainer civerachb@clearpathrobotics.com --pkgsource https://github.com/civerachb-cpr/deg2rad --provides deg2rad,rad2deg --install ./install.sh
+sudo checkinstall -D \
+	--pkgname deg2rad \
+	--pkgversion 0.2.1 \
+	--pkgrelease 1 \
+	--pkglicense bsd \
+	--arch all \
+	--pkggroup devel \
+	--maintainer civerachb@clearpathrobotics.com \
+	--pkgsource https://github.com/civerachb-cpr/deg2rad \
+	--provides deg2rad,rad2deg \
+	--install ./install.sh
 
